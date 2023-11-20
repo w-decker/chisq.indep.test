ef1584d5084aad8d66cc34025c6a25b278737075
Your Name <deckerwill7@gmail.com>
2023-11-19 21:39:03 -0600

    add additional features in R

--- R/chisq.indep.test.R
+++ R/chisq.indep.test.R
@@ -1,16 +1,29 @@
 #' Function to compute chi-squared independent samples test
 #' @param X vector or matrix of data
+#' @param full If TRUE, return structure of additional data. False (defulat)
+#' returns only the statistic.
 #'
 #' @return Named number of chi-squared value
 #'
-#' @example chisq.indep.test(X)
+#' @example
+#' d = chisq.indep.test(X)
 #'
 #' @export
 
-chisq.indep.test <- function(X){
+chisq.indep.test <- function(X, full = FALSE){
   N <- sum(X)
   E <- matrix(data =(rowSums(X) %o% colSums(X)) / N,
               nrow = nrow(X), ncol = ncol(X))
   x2 = sum(((X - E)^2) / E)
-  return(c("Chi-squared" = x2))
+  if (full != FALSE){
+    d = structure(list(
+      observed = X,
+      expected = E,
+      chisq = x2
+    ))
+    return(d)
+  }
+  else {
+    return(c("Chi-squared" = x2))
+  }
 }
